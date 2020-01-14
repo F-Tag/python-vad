5c32ecbd8cf8fa8dbc6f1ef6b3c6250a395c9f82
Fumiaki Taguchi <25476669+F-Tag@users.noreply.github.com>
2020-01-15 00:20:31 +0900

    Refactor vad.py (#5)
    * refactor vad
    
    * use hop_length
    
    * new test
    
    * new test

--- pyvad/vad.py
+++ pyvad/vad.py
@@ -9,7 +9,7 @@ from librosa.core import resample
 from librosa.util import frame
 
 
-def vad(data, fs, fs_vad=16000, hoplength=30, vad_mode=0):
+def vad(data, fs, fs_vad=16000, hop_length=30, vad_mode=0):
     """ Voice activity detection.
     This was implementioned for easier use of py-webrtcvad.
     Parameters
@@ -25,9 +25,9 @@ def vad(data, fs, fs_vad=16000, hoplength=30, vad_mode=0):
         Sampling frequency for webrtcvad.
         fs_vad must be 8000, 16000, 32000 or 48000.
         Default is 16000.
-    hoplength : int, optional
+    hop_length : int, optional
         Step size[milli second].
-        hoplength must be 10, 20, or 30.
+        hop_length must be 10, 20, or 30.
         Default is 0.1.
     vad_mode : int, optional
         set vad aggressiveness.
@@ -46,8 +46,8 @@ def vad(data, fs, fs_vad=16000, hoplength=30, vad_mode=0):
     if fs_vad not in [8000, 16000, 32000, 48000]:
         raise ValueError('fs_vad must be 8000, 16000, 32000 or 48000.')
 
-    if hoplength not in [10, 20, 30]:
-        raise ValueError('hoplength must be 10, 20, or 30.')
+    if hop_length not in [10, 20, 30]:
+        raise ValueError('hop_length must be 10, 20, or 30.')
 
     if vad_mode not in [0, 1, 2, 3]:
         raise ValueError('vad_mode must be 0, 1, 2 or 3.')
@@ -56,16 +56,17 @@ def vad(data, fs, fs_vad=16000, hoplength=30, vad_mode=0):
     if data.dtype.kind == 'i':
         if data.max() > 2**15 - 1 or data.min() < -2**15:
             raise ValueError(
-                'when data type is int, data must be -32768 < data < 32767.')
-        data = data.astype('f')
+                'When data.type is int, data must be -32768 < data < 32767.')
+        data = data.astype('f') / 2.0**15
 
     elif data.dtype.kind == 'f':
-        if np.abs(data).max() >= 1:
-            data = data / np.abs(data).max() * 0.9
-            warn('input data was rescaled.')
-        data = (data * 2**15).astype('f')
+        if np.abs(data).max() > 1:
+            raise ValueError(
+                'When data.type is float, data must be -1.0 <= data <= 1.0.')
+        data = data.astype('f')
+
     else:
-        raise ValueError('data dtype must be int or float.')
+        raise ValueError('data.dtype must be int or float.')
 
     data = data.squeeze()
     if not data.ndim == 1:
@@ -74,12 +75,16 @@ def vad(data, fs, fs_vad=16000, hoplength=30, vad_mode=0):
     # resampling
     if fs != fs_vad:
         resampled = resample(data, fs, fs_vad)
+        if np.abs(resampled).max() > 1.0:
+            resampled *= (0.99 / np.abs(resampled).max())
+            warn('Resampling causes data clipping. data was rescaled.')
+
     else:
         resampled = data
 
-    resampled = resampled.astype('int16')
+    resampled = (resampled * 2.0**15).astype('int16')
 
-    hop = fs_vad * hoplength // 1000
+    hop = fs_vad * hop_length // 1000
     framelen = resampled.size // hop + 1
     padlen = framelen * hop - resampled.size
     paded = np.lib.pad(resampled, (0, padlen), 'constant', constant_values=0)
@@ -89,7 +94,7 @@ def vad(data, fs, fs_vad=16000, hoplength=30, vad_mode=0):
     vad.set_mode(vad_mode)
     valist = [vad.is_speech(tmp.tobytes(), fs_vad) for tmp in framed]
 
-    hop_origin = fs * hoplength // 1000
+    hop_origin = fs * hop_length // 1000
     va_framed = np.zeros([len(valist), hop_origin])
     va_framed[valist] = 1
 
--- test/vad_test.py
+++ test/vad_test.py
@@ -4,20 +4,44 @@
 from itertools import product
 
 from librosa import load
+
 from pyvad import vad
 
 fs_vads = (8000, 16000, 32000, 48000)
 hops = (10, 20, 30)
 vad_modes = (0, 1, 2, 3)
+fss = [16000, 22050]
 
 name = "voice/arctic_a0007.wav"
-data, fs = load(name, sr=None)
 
-for fs_vad, hop, vad_mode in product(fs_vads, hops, vad_modes):
-    vact = vad(data, fs, fs_vad=16000, hoplength=30, vad_mode=0)
+for fs in fss:
+
+    data, fs_r = load(name, sr=fs)
+    for fs_vad, hop, vad_mode in product(fs_vads, hops, vad_modes):
+        # print(fs, fs_vad, hop, vad_mode)
+        vact = vad(data, fs_r, fs_vad=fs_vad,
+                   hop_length=hop, vad_mode=vad_mode)
+        assert vact.sum() > data.size//2, vact.sum()
+
+        """
+        import matplotlib.pyplot as plt
+        plt.plot(data)
+        plt.plot(vact)
+        plt.savefig(("voice_"+str(fs_r)+str(fs_vad)+str(hop)+str(vad_mode)+".png"))
+        plt.close()
+        """
+
+    """
+    data = (np.random.rand(fs*3)-0.5)*0.1
+    for fs_vad, hop, vad_mode in product(fs_vads, hops, vad_modes):
+        print(fs, fs_vad, hop, vad_mode)
+        vact = vad(data, fs, fs_vad=fs_vad, hop_length=hop, vad_mode=vad_mode)
+        # assert not vact.any(), vact.sum()
 
 
-data, fs = load(name, sr=22050)
-
-for fs_vad, hop, vad_mode in product(fs_vads, hops, vad_modes):
-    vact = vad(data, fs, fs_vad=16000, hoplength=30, vad_mode=0)
+        import matplotlib.pyplot as plt
+        plt.plot(data)
+        plt.plot(vact)
+        plt.savefig(("noise_"+str(fs)+str(fs_vad)+str(hop)+str(vad_mode)+".png"))
+        plt.close()
+    """
